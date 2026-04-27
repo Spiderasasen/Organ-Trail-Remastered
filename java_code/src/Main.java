5d2eaf278c906dc31cc7e2623fd772f701b73d11
spiderasasen <spiderasasen@gmail.com>
2026-04-27 08:08:56 -0400

    got the main layout ready, now to make the file reader

--- java_code/src/Main.java
+++ java_code/src/Main.java
@@ -1,5 +1,46 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 class Main{
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        //getting the scanner
+        Scanner scan = new Scanner(System.in);
+
+        //main vars
+        boolean flag = false;
+        String[] options = {"1. Play", "2. How to Play", "3. Exit"};
+
+        //main game section
+        while (!flag){
+            System.out.println("-- Organ Trail Remastered --\n" +
+                    "Please select an option:");
+            for (String option : options){
+                System.out.println(option);
+            }
+
+            //seeing what the user chosed
+            try{
+                int choice = scan.nextInt();
+                switch (choice){
+                    case 1:
+                        System.out.println("Playing the game\n");
+                        break;
+                    case 2:
+                        System.out.println("How to play\n");
+                        break;
+                    case 3:
+                        System.out.println("Exiting the game...\n");
+                        flag = true;
+                        break;
+                    default:
+                        System.out.println("Invalid choice.\n");
+                        break;
+                }
+            }
+            catch (InputMismatchException e){
+                System.out.println("Please enter a number\n");
+                scan.nextLine();
+            }
+        }
     }
 }
